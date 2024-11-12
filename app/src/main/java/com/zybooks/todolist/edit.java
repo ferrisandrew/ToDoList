4bdd8d2e732ce6b7f736af4e37c6f9bf1bb20b68
ferrisandrew <163781511+ferrisandrew@users.noreply.github.com>
2024-11-11 22:41:18 -0500

    edit.java and small changes to mainactivity

---
+++ app/src/main/java/com/zybooks/todolist/edit.java
@@ -0,0 +1,11 @@
+package com.zybooks.todolist;
+
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+public class edit {
+    private EditText Input;
+    private DatePicker Date;
+    private Button SaveButton;
+}
