9a49bebc7b811bc8efb80f4526208e6ae7572115
Nighthawk500 <jafm0108@gmail.com>
2024-11-14 16:43:48 -0500

    mostly completed main + changed button var name

--- app/src/main/java/com/zybooks/todolist/MainActivity.java
+++ app/src/main/java/com/zybooks/todolist/MainActivity.java
@@ -1,26 +1,27 @@
 package com.zybooks.todolist;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import com.zybooks.todolist.DatabaseHelper;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
-    private Button AddItem;
+    private Button addItemButton;
     private DatabaseHelper dbHelper;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> itemList;
+    private ArrayList<Integer> itemIds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
         dbHelper = new DatabaseHelper(this);
         listView = findViewById(R.id.listView);
-        AddItem = findViewById(R.id.AddItem);
+        addItemButton = findViewById(R.id.addItemButton);
 
-        AddItem.setOnClickListener(new View.OnClickListener() {
+        itemList = new ArrayList<>();
+        itemIds = new ArrayList<>();
+
+        loadItems();
+
+        addItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, EditActivity.class);
@@ -39,5 +45,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemId = itemIds.get(position);
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                intent.putExtra("ITEM_ID", itemId);
+                startActivity(intent);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemId = itemIds.get(position);
+                dbHelper.deleteItem(itemId);
+                loadItems();
+                Toast.makeText(MainActivity.this, "Item deleted", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void loadItems() {
+        itemList.clear();
+        itemIds.clear();
+        Cursor cursor = dbHelper.getAllItems();
+        if (cursor.moveToFirst()) {
+            do {
+                String name = cursor.getString(cursor.getColumnIndex("name"));
+                String date = cursor.getString(cursor.getColumnIndex("date"));
+                int completed = cursor.getInt(cursor.getColumnIndex("completed"));
+                String itemText = name + " - " + date + (completed == 1 ? " (Completed)" : "");
+                itemList.add(itemText);
+                itemIds.add(cursor.getInt(cursor.getColumnIndex("id")));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemList);
+        listView.setAdapter(adapter);
     }
 }
