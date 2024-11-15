2519347584f2fd24a55b5b6f1536ba337be65928
ferrisandrew <163781511+ferrisandrew@users.noreply.github.com>
2024-11-15 12:55:09 -0500

    easy stuff out of the way for editactivity

--- app/src/main/java/com/zybooks/todolist/EditActivity.java
+++ app/src/main/java/com/zybooks/todolist/EditActivity.java
@@ -1,6 +1,7 @@
 package com.zybooks.todolist;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -12,9 +13,26 @@ public class EditActivity extends AppCompatActivity {
     private EditText Input;
     private DatePicker Date;
     private Button SaveButton;
+    private DatabaseHelper dbHelper;
+    private int ItemID = -1;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        SaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = Input.getText().toString();
+                String date = Date.getYear() + "-" + (Date.getMonth() + 1) + "-" + Date.getDayOfMonth();
+
+                if (ItemID == -1) {
+                    dbHelper.insertItem(new ToDoItem(name, date, false));
+                } else {
+                    dbHelper.updateToDo(new ToDoItem(ItemID, name, date, false));
+                }
+                finish();
+            }
+        });
     }
 }
